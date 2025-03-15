8b6c88127d51dea163d0acda1d460282129a0d99
Somnath Das <somnath8das@gmail.com>
2025-03-15 13:07:22 +0530

    ui-refactor-create-post

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -6,7 +6,6 @@ import {
   ScrollView,
   Text,
   TouchableOpacity,
-  ActivityIndicator,
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
--- src/app/(tabs)/new_post.tsx
+++ src/app/(tabs)/new_post.tsx
@@ -1,5 +1,130 @@
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import CustomButton from "~/src/components/CustomButton";
+import { uploadImage } from "~/src/lib/cloudinary";
+import { useRouter } from "expo-router";
+import { supabase } from "~/src/lib/superbase";
+import { useAuth } from "~/src/global/useAuth";
+import { AuthContextType } from "~/src/types";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { SafeAreaView } from "react-native-safe-area-context";
+import InputField from "~/src/components/InputField";
 
 export default function NewPost() {
-  return <Text>NewPost</Text>;
+  const router = useRouter();
+  const { auth, updateAuth } = useAuth() as AuthContextType;
+  const [caption, setCaption] = useState("");
+  const [media, setMedia] = useState<string | null>("");
+  const [mediaType, setMediaType] = useState<
+    "video" | "image" | "livePhoto" | "pairedVideo" | undefined
+  >();
+  const player = useVideoPlayer(media || "", (player) => {
+    player.loop = true;
+    player.play();
+  });
+  useEffect(() => {
+    if (!media) {
+      pickMedia();
+    }
+  }, [media]);
+
+  const pickMedia = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.5, // cloudinary free plan limit 1mb of any file
+    });
+
+    if (!result.canceled) {
+      setMedia(result.assets[0].uri);
+      setMediaType(result.assets[0].type);
+    }
+  };
+
+  const createPost = async () => {
+    let response;
+    if (media) {
+      response = await uploadImage(media);
+    }
+
+    const { data, error } = await supabase
+      .from("posts")
+      .insert([
+        {
+          caption,
+          image: response?.public_id,
+          user_id: auth.session?.user.id,
+          media_type: mediaType,
+        },
+      ])
+      .select();
+
+    router.push("/(tabs)");
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: "center", padding: 2 }}>
+      {!media ? (
+        <View
+          style={{
+            width: 150,
+            aspectRatio: 3 / 4,
+            borderRadius: 10,
+            backgroundColor: "#bababa",
+          }}
+        />
+      ) : mediaType === "image" ? (
+        <Image
+          source={{ uri: media }}
+          style={{
+            width: 150,
+            aspectRatio: 3 / 4,
+            borderRadius: 10,
+            backgroundColor: "#bababa",
+          }}
+        />
+      ) : (
+        <VideoView
+          style={{ width: "90%", height: 250, marginTop: 20 }}
+          player={player}
+          allowsFullscreen
+          allowsPictureInPicture
+        />
+      )}
+      <TouchableOpacity onPress={pickMedia}>
+        <Text style={{ fontWeight: "500", fontSize: 16, margin: 8 }}>
+          Change
+        </Text>
+      </TouchableOpacity>
+      <View
+        style={{
+          marginTop: "20%",
+          width: "100%",
+          alignItems: "center",
+          gap: 16,
+        }}
+      >
+        <InputField
+          title="Post"
+          placeholder="What is on your mind ?"
+          value={caption}
+          handleChangeText={(e) => {
+            setCaption(e);
+          }}
+          keyboardType="default"
+        />
+
+        <CustomButton title="Share" onPress={createPost} />
+      </View>
+    </SafeAreaView>
+  );
 }
