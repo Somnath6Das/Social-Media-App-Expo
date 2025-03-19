51c0aee67779e36d7574661876048d2b8ab12d01
Somnath Das <somnath8das@gmail.com>
2025-03-19 20:28:24 +0530

    comment-ui

--- src/components/CommandView.tsx
+++ src/components/CommandView.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function CommandView() {
   return (
-    <View>
-      <Text>Command</Text>
-    </View>
+    <ScrollView>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          marginLeft: 10,
+          alignItems: "center",
+          gap: 10,
+        }}
+      >
+        <Image
+          source={require("~/assets/photos/user.png")}
+          style={{ width: 35, height: 35, aspectRatio: 1, borderRadius: 50 }}
+        />
+
+        <Text style={{ fontSize: 15, fontWeight: "500" }}>Username</Text>
+      </View>
+      <View style={{ marginLeft: 55, marginRight: 6 }}>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          possimus tenetur vel assumenda, temporibus nulla, tempore at explicabo
+          enim libero minima id deserunt nobis minus molestias dolores! Error,
+          itaque reiciendis?
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
