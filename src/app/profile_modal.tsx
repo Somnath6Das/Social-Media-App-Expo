e40ac4c67326f872eeef67d129decc2ac66f07d7
Somnath Das <somnath8das@gmail.com>
2025-03-28 03:41:27 +0530

    profile-model-data

--- src/app/profile_modal.tsx
+++ src/app/profile_modal.tsx
@@ -1,18 +1,30 @@
 import { Link, router, useLocalSearchParams } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { useTheme } from "../theme/ThemeProvider";
 import { Ionicons } from "@expo/vector-icons";
+import { supabase } from "../lib/superbase";
+import { useEffect } from "react";
 
 export default function Modal() {
   const isPresented = router.canGoBack();
   const { postId } = useLocalSearchParams();
   const theme = useTheme();
 
+  const getProfile = async () => {
+    let { data, error } = await supabase
+      .from("posts")
+      .select("*, profiles(*)")
+      .eq("id", postId);
+    console.log(JSON.stringify(data, null, 2));
+  };
+  useEffect(() => {
+    getProfile();
+  }, []);
   return (
     <View style={styles.container}>
       {isPresented && (
         <Link style={{ color: theme.text }} href="../">
-          <Ionicons name="arrow-back" size={24} color="black" />
+          <Ionicons name="arrow-back" size={24} color={theme.text} />
         </Link>
       )}
       <Text style={{ color: theme.text }}>{postId}</Text>
