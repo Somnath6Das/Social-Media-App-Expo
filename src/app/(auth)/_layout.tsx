65aba2e7617f6337be15c651da7b2baa9d0d5996
Somnath Das <somnath8das@gmail.com>
2025-03-29 20:40:23 +0530

    update-status-bar

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,8 +1,9 @@
 import { Redirect, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { useAuth } from "~/src/global/useAuth";
 import { useTheme } from "~/src/theme/ThemeProvider";
 import { AuthContextType } from "~/src/types";
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function Layout() {
   const theme = useTheme();
   const { auth, updateAuth } = useAuth() as AuthContextType;
@@ -10,18 +11,24 @@ export default function Layout() {
     return <Redirect href="/(tabs)" />;
   }
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: {
-          backgroundColor: theme.background,
-        },
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="verify_email" />
-      <Stack.Screen name="verify_otp" />
-      <Stack.Screen name="set_password" />
-    </Stack>
+    <GestureHandlerRootView>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            backgroundColor: theme.background,
+          },
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="verify_email" />
+        <Stack.Screen name="verify_otp" />
+        <Stack.Screen name="set_password" />
+      </Stack>
+      <StatusBar
+        backgroundColor={theme.background}
+        style={theme.background === "#212121" ? "light" : "dark"}
+      />
+    </GestureHandlerRootView>
   );
 }
