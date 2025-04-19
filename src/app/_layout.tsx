2012ea00f2a6d3392262f065ed210e4ed2430283
Somnath Das <somnath8das@gmail.com>
2025-04-20 00:06:26 +0530

    update-notification-icon-splash-load

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,16 @@
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
 import { ThemeProvider } from "../theme/ThemeProvider";
 import { useTheme } from "../theme/ThemeProvider";
 import { ActivityIndicator, Alert, AppState, View } from "react-native";
 import { supabase } from "../lib/superbase";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useAuth } from "../global/useAuth";
 import { AuthContextType } from "../types";
 import NetworkAware from "../components/networkAware";
+
+// splash screen
+SplashScreen.preventAutoHideAsync();
 
 AppState.addEventListener("change", (state) => {
   if (state === "active") {
@@ -17,6 +21,11 @@ AppState.addEventListener("change", (state) => {
 });
 
 export default function RootLayout() {
+  // splash screen
+  const onLayoutRootView = useCallback(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
   const isMounted = useRef(false);
   const theme = useTheme();
   const { auth, updateAuth } = useAuth() as AuthContextType;
@@ -48,38 +57,42 @@ export default function RootLayout() {
   }, []);
   if (!auth.isReady) {
     return (
-      <NetworkAware>
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <ActivityIndicator size="large" color="#ecaf0a" />
-        </View>
-      </NetworkAware>
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <NetworkAware>
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <ActivityIndicator size="large" color="#ecaf0a" />
+          </View>
+        </NetworkAware>
+      </View>
     );
   }
 
   return (
-    <NetworkAware>
-      <ThemeProvider>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-            contentStyle: {
-              backgroundColor: theme.background,
-            },
-          }}
-        >
-          <Stack.Screen
-            name="profile_modal"
-            options={{
-              presentation: "transparentModal",
-              animation: "fade",
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NetworkAware>
+        <ThemeProvider>
+          <Stack
+            screenOptions={{
               headerShown: false,
+              contentStyle: {
+                backgroundColor: theme.background,
+              },
             }}
-          />
-          <Stack.Screen name="index" />
-        </Stack>
-      </ThemeProvider>
-    </NetworkAware>
+          >
+            <Stack.Screen
+              name="profile_modal"
+              options={{
+                presentation: "transparentModal",
+                animation: "fade",
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen name="index" />
+          </Stack>
+        </ThemeProvider>
+      </NetworkAware>
+    </View>
   );
 }
